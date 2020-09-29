f1c8fef1a5941f3494420aabd35869a69fc46bee
Guirlande <damienpoulain@gmail.com>
2020-09-29 19:38:55 +0200

    Modified script to take into account randomly ordered queues-and-status output

--- intelmq_status.py
+++ intelmq_status.py
@@ -26,26 +26,29 @@ def intelmq_status():
             continue
         if bot_search:
             temp_bot['botname'] = bot_search.group(1)
-        else:
-            for bots in bot_status:
-                try:
-                    bot = bots["botname"]
-                    queue_regex = rf"\b(?=\w){bot}\b(?!\w)-(.*) - ([0-9]*)"
-                    queue_search = re.search(queue_regex,line)
-                    if queue_search:
-                        bots[queue_search.group(1)] = int(queue_search.group(2))
-                except Exception:
-                    pass
-            continue
-
-
         status_search = re.search(status_regex,line)
         if status_search:
             temp_bot['status'] = 1
         else:
-            temp_bot['status'] = 0
+            if bot_search:
+                temp_bot['status'] = 0
+        if temp_bot == {}:
+            continue
+        bot_status.append(temp_bot)
 
         bot_status.append(temp_bot)
+    for line in output.splitlines():
+        for bots in bot_status:
+            try:
+                bot = bots["botname"]
+                queue_regex = rf"\b(?=\w){bot}\b(?!\w)-(.*) - ([0-9]*)"
+                queue_search = re.search(queue_regex,line)
+                if queue_search:
+                    bots[queue_search.group(1)] = int(queue_search.group(2))
+            except Exception:
+                pass
+        continue
+
     return bot_status
 
 
